241368c3002bfd0e508b49936c5287f2b7ebb396
Victor Pires <vctprs@gmail.com>
2023-05-18 23:58:12 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Container, Grid } from "@mui/material";
 import { Navbar } from "./components/Navbar";
 import Result from "./components/Result";
@@ -5,6 +6,15 @@ import SliderSelect from "./components/SliderSelect";
 import TenureSelect from "./components/TenureSelect";
 
 function App() {
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000,
+    loanAmount: 3000,
+    loanTerm: 3000,
+  })
+
+
+
   return (
     <div className="App">
       <Navbar />
